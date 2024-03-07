f33ae81fadda681029a68d13245c29b303033328
olusiekwin <olusiekwin@gmail.com>
2024-03-08 02:41:12 +0300

    ES6_Basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function iterateThroughObject(reportWithIterator) {
+    let result = "";
+  let employee = reportWithIterator.next();
+    while (!employee.done)
+    {
+      result += employee.value;
+      employee = reportWithIterator.next();
+      if (!employee.done)
+        result += (" | ");
+    }
+    return result;
+   }
---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const budget = {};
+  
+    return {
+      [`income-${getCurrentYear()}`]: income,
+      [`gdp-${getCurrentYear()}`]: gdp,
+      [`capita-${getCurrentYear()}`]: capita,
+    }
+  }
