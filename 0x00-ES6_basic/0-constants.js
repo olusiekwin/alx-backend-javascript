8e540e827c09186cd8de295c2b98e2dcc333e804
olusiekwin <olusiekwin@gmail.com>
2024-03-08 02:59:16 +0300

    Task 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,14 @@
-// 0-constants.js
-
+/* eslint-disable */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combo = `But sometimes let${getLast()}`;
+  return combo;
+}
