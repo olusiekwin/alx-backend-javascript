9b3564326d80b2d0c8b879b0b5bc4599599cb6aa
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:18:51 +0300

    Task-3

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,11 @@
-export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
-  
-    return [task, task2];
-  }
-  
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
