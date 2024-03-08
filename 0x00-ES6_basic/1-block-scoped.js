22bcc132a7c591a0ee3ae0e9a1209798fada25a6
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:18:34 +0300

    Task-2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
   }
-  
+
+  return [task, task2];
+}
