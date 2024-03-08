efafc9984bbfa148c02abc098cf2d529fff44b11
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:45:51 +0300

    Task-4~

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
+}
