ce161582c9dc6aba63f785a0ba726fc716324162
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:32:56 +0300

    Task-12

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [`${departmentName}`]: employees,
+  }
+
+}
+
