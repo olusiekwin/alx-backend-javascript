ec4f403074b4993880f03fbe60a60cf9fddcc55f
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:50:22 +0300

    Task-7

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
+
