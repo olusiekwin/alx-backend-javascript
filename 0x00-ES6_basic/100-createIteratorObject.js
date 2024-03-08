790c35dbed83e124a59ba74946a7ce0660c413d9
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:34:38 +0300

    Advanced Tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function createIteratorObject(report) {
-    let emp = [];
-  for (const [department, employees] of Object.entries(report)) {
-    for (const [key, value] of Object.entries(employees)){
-      for (let b of value){
-      emp.push(b);
-      }
+  let emp = [];
+for (const [department, employees] of Object.entries(report)) {
+  for (const [key, value] of Object.entries(employees)){
+    for (let b of value){
+    emp.push(b);
     }
   }
-  let iterEmp = emp[Symbol.iterator]();
-  return iterEmp;
-  }
+}
+let iterEmp = emp[Symbol.iterator]();
+return iterEmp;
+}
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function iterateThroughObject(reportWithIterator) {
-    let result = "";
-  let employee = reportWithIterator.next();
-    while (!employee.done)
-    {
-      result += employee.value;
-      employee = reportWithIterator.next();
-      if (!employee.done)
-        result += (" | ");
-    }
-    return result;
-   }
+  let result = "";
+let employee = reportWithIterator.next();
+  while (!employee.done)
+  {
+    result += employee.value;
+    employee = reportWithIterator.next();
+    if (!employee.done)
+      result += (" | ");
+  }
+  return result;
+ }
