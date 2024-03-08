61f73657fffdf361e4578b2ec3c28bd4d7c12f64
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:52:18 +0300

    Task-9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function guardrail(mathFunction){
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
+  queue.push("Guardrail was processed");
+  return queue;
+}
