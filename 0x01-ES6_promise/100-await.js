1f33f7aff954d545ce2e4432cf3780946c6cf028
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:54:02 +0300

    Advanced-Task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
