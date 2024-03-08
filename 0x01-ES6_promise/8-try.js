c90c65539a3103c662105f2b2e9bbe69b2a88951
olusiekwin <olusiekwin@gmail.com>
2024-03-08 04:51:05 +0300

    Task-8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0)
+      throw Error('cannot divide by 0');
+    return numerator/denominator;
+}
